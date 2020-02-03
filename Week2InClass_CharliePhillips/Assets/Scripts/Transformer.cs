d37633418445760ea4cd005098284fa251c61304
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-03 13:55:21 -0600

    serialized private fields!

--- Week2InClass_CharliePhillips/Assets/Scripts/Transformer.cs
+++ Week2InClass_CharliePhillips/Assets/Scripts/Transformer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Transformer : MonoBehaviour
 {
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float angularSpeed = 45f;
+
     void Start()
     {
         
@@ -11,7 +14,7 @@ public class Transformer : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(0, 0, 1);
-        transform.Rotate(0, 0, 45f);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+        transform.Rotate(0, 0, angularSpeed * Time.deltaTime);
     }
 }
