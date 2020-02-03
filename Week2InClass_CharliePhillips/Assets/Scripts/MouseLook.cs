cf60d5ed7d13be586a7f9dcdadf2e08be192f415
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-03 15:07:05 -0600

    mouse look and level design

--- Week2InClass_CharliePhillips/Assets/Scripts/MouseLook.cs
+++ Week2InClass_CharliePhillips/Assets/Scripts/MouseLook.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     [SerializeField] float mouseSensitivityX = 1;
     [SerializeField] float mouseSensitivityY = 1;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
@@ -23,6 +22,11 @@ public class MouseLook : MonoBehaviour
         float rotationY = transform.eulerAngles.y + deltaMouseX; //euler and quaternion are the 2 types of angles
         float rotationX = transform.eulerAngles.x - deltaMouseY;
 
-        transform.eulerAngles = new Vector3(rotationX, rotationY, 0); 
+        transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
