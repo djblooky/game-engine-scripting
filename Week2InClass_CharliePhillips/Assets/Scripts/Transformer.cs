2b976d0f1c06edbd7bfd2362f4c399c1a1582bcd
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-03 13:39:41 -0600

    transform practice

---
+++ Week2InClass_CharliePhillips/Assets/Scripts/Transformer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformer : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(0, 0, 1);
+        transform.Rotate(0, 0, 45f);
+    }
+}
