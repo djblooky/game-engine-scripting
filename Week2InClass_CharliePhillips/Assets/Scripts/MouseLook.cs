86f179b6a90dd0aeff3362b0edea7f4779929fdd
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-10 13:21:28 -0600

    week3 assets import

--- Week2InClass_CharliePhillips/Assets/Scripts/MouseLook.cs
+++ Week2InClass_CharliePhillips/Assets/Scripts/MouseLook.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    // Serialize private fields to make them show up in the inspector
+    // instead of using the "public" access modifier.
     [SerializeField] float mouseSensitivityX = 1;
     [SerializeField] float mouseSensitivityY = 1;
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;    
     }
 
     void Update()
     {
-        float deltaMouseX = Input.GetAxis("Mouse X");
-        float deltaMouseY = Input.GetAxis("Mouse Y");
+        // Get the amount the cursor has moved this tick of the engine.
+        float deltaMouseX = Input.GetAxis("Mouse X") * mouseSensitivityX;
+        float deltaMouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        //Debug.Log($"X: {deltaMouseX}\t Y: {deltaMouseY}");
 
-        Debug.Log($"deltaMouseX:{deltaMouseX} \t deltaMouseY:{deltaMouseY}");
-
-        float rotationY = transform.eulerAngles.y + deltaMouseX; //euler and quaternion are the 2 types of angles
+        // Note: the y axis needs to be flipped to have non-inverted controls.
+        float rotationY = transform.eulerAngles.y + deltaMouseX;
         float rotationX = transform.eulerAngles.x - deltaMouseY;
 
         transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
@@ -27,6 +30,10 @@ public class MouseLook : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
 }
