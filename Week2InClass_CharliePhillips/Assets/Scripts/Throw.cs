b3bf1450e690b8640d22a87205e28c8c923bf363
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-10 13:44:22 -0600

    arranged food on table

---
+++ Week2InClass_CharliePhillips/Assets/Scripts/Throw.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       if( Input.GetMouseButtonDown(0))
+        {
+            //throw
+        }
+    }
+}
