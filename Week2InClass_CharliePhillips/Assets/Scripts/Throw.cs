c6279e2ad6ed14c70f72cc8c9724702c22b5d5ca
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-10 14:40:40 -0600

    arranged table food

--- Week2InClass_CharliePhillips/Assets/Scripts/Throw.cs
+++ Week2InClass_CharliePhillips/Assets/Scripts/Throw.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
-
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float throwStrength = 20;
+    [SerializeField] private float upwardArcInDegrees = 30;
 
     void Update()
     {
        if( Input.GetMouseButtonDown(0))
         {
-            //throw
+            Debug.Log("Throwing!");
+            Quaternion randomRotation = Random.rotationUniform;
+            Vector3 spawnPosition = transform.position + transform.forward * 0.5f;
+            GameObject projectileInstance = Instantiate(projectile, spawnPosition, randomRotation); //instatiates projectile 
+
+            Rigidbody rb = projectileInstance.GetComponent<Rigidbody>(); //gets rigidboy, assigns to rb
+
+            float upwardArcAngle = Mathf.Deg2Rad * upwardArcInDegrees;
+            Vector3 direction = Vector3.RotateTowards(transform.forward, Vector3.up, upwardArcAngle, 0); //assign an arc curve direction
+
+            rb.AddForce(direction * throwStrength, ForceMode.Impulse); //forces projectile away from player
         }
     }
 }
