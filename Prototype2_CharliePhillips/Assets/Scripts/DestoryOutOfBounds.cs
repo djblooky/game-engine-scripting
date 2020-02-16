641c8b0e2afc49441f12edb9ab3d7493b26bc8e5
Haley Phillips <haleyphillips928@gmail.com>
2020-02-15 18:47:12 -0600

    completed 2.3

--- Prototype2_CharliePhillips/Assets/Scripts/DestoryOutOfBounds.cs
+++ Prototype2_CharliePhillips/Assets/Scripts/DestoryOutOfBounds.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DestoryOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
 
     void Update()
     {
-        
+        if(transform.position.z > topBound)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (transform.position.z < lowerBound)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
