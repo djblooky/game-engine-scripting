dfb0f6092c2eab7ccb7da300b0cb8c09434f04a9
Haley Phillips <haleyphillips928@gmail.com>
2020-02-16 13:04:06 -0600

    fixed colliders and rigidbodies

--- Prototype1_CharliePhillips/Assets/Scripts/ObstacleManager.cs
+++ Prototype1_CharliePhillips/Assets/Scripts/ObstacleManager.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] obstaclePrefabs;
+
+    private void Start()
     {
-        
+        SpawnObstacles();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnObstacles()
     {
-        
+        for (int i = 0; i < 100; i++)
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+            float xPos = Random.Range(-GetComponent<Collider>().bounds.size.x, GetComponent<Collider>().bounds.size.x);
+            float yPos = Random.Range(-GetComponent<Collider>().bounds.size.y, GetComponent<Collider>().bounds.size.y);
+            float zPos = Random.Range(-GetComponent<Collider>().bounds.size.z, GetComponent<Collider>().bounds.size.z);
+
+            Instantiate(obstaclePrefabs[obstacleIndex], new Vector3(xPos, yPos, zPos), obstaclePrefabs[obstacleIndex].transform.rotation);
+        }
     }
+
+    private void Update()
+    {
+
+    }
+
+
 }
