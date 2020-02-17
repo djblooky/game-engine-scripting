6705c8438112356c59e9024d721a519b25b15b89
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-17 14:10:20 -0600

    implemented scoreManager

--- Week2InClass_CharliePhillips/Assets/Scripts/Minigame/CollisionTracker.cs
+++ Week2InClass_CharliePhillips/Assets/Scripts/Minigame/CollisionTracker.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class CollisionTracker : MonoBehaviour
 {
     [SerializeField] GameObject cloudParticles;
+    private ScoreManager scoreManager;
+
+    private void Start()
+    {
+        //look up the player
+        //GameObject.Find("player"); //slow and fragile
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        //get the score manager
+        scoreManager = player.GetComponent<ScoreManager>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {   
@@ -16,6 +27,10 @@ public class CollisionTracker : MonoBehaviour
             ContactPoint contact = collision.GetContact(0);
             Instantiate(cloudParticles, contact.point, cloudParticles.transform.rotation);
             Destroy(gameObject);
+
+            //call score manager to add to score
+            scoreManager.IncrementScore();
         }
+
     }
 }
--- Week2InClass_CharliePhillips/Assets/Scripts/Minigame/Player/ScoreManager.cs
+++ Week2InClass_CharliePhillips/Assets/Scripts/Minigame/Player/ScoreManager.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Text scoreText;
+    private int score;
+
+    private void Start()
     {
-        
+        score = 0;
+        UpdateScoreText();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void IncrementScore()
     {
-        
+        score++;
+        UpdateScoreText();
     }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
 }
