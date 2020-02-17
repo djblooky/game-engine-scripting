3b7765659ae40103262e2e46a8d133eb15cd1a49
Haley Phillips <haleyphillips928@gmail.com>
2020-02-16 20:08:09 -0600

    finished tutorial

---
+++ Week2InClass_CharliePhillips/Assets/Scripts/CheckForRestart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckForRestart : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Minigame");
+        }
+    }
+}
--- Week2InClass_CharliePhillips/Assets/Scripts/Throw.cs
+++ Week2InClass_CharliePhillips/Assets/Scripts/Throw.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
-    private List<GameObject> foodList;
+    [SerializeField] GameObject[] foodList;
 
     [SerializeField] private GameObject projectile;
     [SerializeField] private float throwStrength = 20;
     [SerializeField] private float upwardArcInDegrees = 30;
-    [SerializeField] private int throwsLeft = 5;
-
-    private void Awake()
-    {
-        foodList = Resources.LoadAll<GameObject>("Food Kit FBX").ToList();
-    }
+    [SerializeField] private int throwsLeft = 6;
 
     void Update()
     {
@@ -27,13 +22,14 @@ public class Throw : MonoBehaviour
 
     private void SetRandomFood()
     {
-        projectile = foodList[Random.Range(0, foodList.Count)];
+        projectile = foodList[Random.Range(0, foodList.Length)];
     }
 
     private void CheckForThrow()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            throwsLeft--;
             SetRandomFood();
             Debug.Log("Throwing!");
             Quaternion randomRotation = Random.rotationUniform;
@@ -46,6 +42,7 @@ public class Throw : MonoBehaviour
             Vector3 direction = Vector3.RotateTowards(transform.forward, Vector3.up, upwardArcAngle, 0); //assign an arc curve direction
 
             rb.AddForce(direction * throwStrength, ForceMode.Impulse); //forces projectile away from player
+            Destroy(projectileInstance.gameObject, 10); //destroy after 10sec
         }
     }
 }
