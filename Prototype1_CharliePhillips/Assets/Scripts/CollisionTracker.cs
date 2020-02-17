27d1887c2eaa0151d4a9a1b7b2a9e5761b328140
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-17 15:04:39 -0600

    started week4 homework
    need to fix collisiontracker and link dmagemanager

---
+++ Prototype1_CharliePhillips/Assets/Scripts/CollisionTracker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CollisionTracker : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log($"Hit obstacle {gameObject.name}");
+        }
+    }
+}
---
+++ Prototype1_CharliePhillips/Assets/Scripts/DamageManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageManager : MonoBehaviour
+{
+    private int damage;
+    [SerializeField] Text damageText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = 0;
+        UpdateDamageText();
+    }
+
+    public void IncrementDamage()
+    {
+        damage++;
+        UpdateDamageText();
+    }
+
+    private void UpdateDamageText()
+    {
+        damageText.text = $"Damage: {damage}";
+    }
+}
