05e7a93e108b0abcefa9d4aa7432011ef4937c43
Haley Phillips <haleyphillips928@gmail.com>
2020-02-23 23:37:40 -0600

    set jump, corrected moveLeft.cs

--- Prototype3_CharliePhillips/Assets/Scripts/MoveLeft.cs
+++ Prototype3_CharliePhillips/Assets/Scripts/MoveLeft.cs
@@ -18,7 +18,7 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(playerControllerScript.gameOver == true)
+        if(playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
