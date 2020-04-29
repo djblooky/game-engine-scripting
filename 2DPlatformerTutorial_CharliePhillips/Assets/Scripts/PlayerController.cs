aa32531f5caad0c0e5475b7a7689dff082e6d222
Haley Phillips <haleyphillips928@gmail.com>
2020-04-28 23:39:31 -0500

    player controller left/right movement

--- 2DPlatformerTutorial_CharliePhillips/Assets/Scripts/PlayerController.cs
+++ 2DPlatformerTutorial_CharliePhillips/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[DisallowMultipleComponent]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Ground Movement")]
+    [Tooltip("Movement speed in tiles per second (1 tile = 1 meter)")]
+    [SerializeField]
+    private float speed;
+
+    [Header("Air Movement")]
+    [Tooltip("The upward force applied when player jumps")]
+    [Range(0f, 10f)]
+    [SerializeField]
+    private float jumpForce;
+
+    private Rigidbody2D playerRB;
+    private bool isJumping;
+    private bool isFacingRight = true;
+
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal"); //num between -1,1 (-1=left key, 1=right key)
+        float xVelocity = horizontalInput * speed;
+        playerRB.velocity = new Vector2(xVelocity, playerRB.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        //put physics code in here!
     }
 }
